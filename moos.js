13e1a0f1fd14f1fc76e0c97cb5c11ee09b122601
Toomas Lõõnik <toomloo@greeny.cs.tlu.ee>
2016-02-08 11:01:45 +0200

    added more jam

--- moos.js
+++ moos.js
@@ -1,1 +1,36 @@
-//
+(function(){
+  "use script";
+
+  var Moosipurk = function(){
+
+    //see on singleton pattern
+    if(Moosipurk.instance){
+      return Moosipurk.instance;
+    }
+
+    //this viitab moosipurgi funktsioonile
+    Moosipurk.instance = this;
+
+    console.log("Moosipurgi sees");
+    
+
+    //kui tahan moosipurgile referenci siis kasuton THIS = MOOSIPURGI RAKENDUS ISE
+    this.init();
+  };
+
+  //kõik funktsioonid lähevad moosipurgi külge
+  Moosipurk.prototype = {
+    init: function(){
+      console.log("Rakendus läks tööle");
+
+    },
+    bindMouseEvent: function(){
+
+    }
+  };
+
+  var app = new Moosipurk();
+
+
+
+})();

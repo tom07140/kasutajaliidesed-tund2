428443ca3efbb12c90924f8d7dd0ca63101ed682
Toomas Lõõnik <toomloo@greeny.cs.tlu.ee>
2016-02-08 11:24:18 +0200

    loeb moose

--- moos.js
+++ moos.js
@@ -12,24 +12,39 @@
     Moosipurk.instance = this;
 
     console.log("Moosipurgi sees");
-    
+
+    this.click_count = 0;
+    console.log(this);
 
     //kui tahan moosipurgile referenci siis kasuton THIS = MOOSIPURGI RAKENDUS ISE
     this.init();
+
   };
+
+  window.Moosipurk = Moosipurk; //paneme muutuja külge
 
   //kõik funktsioonid lähevad moosipurgi külge
   Moosipurk.prototype = {
     init: function(){
       console.log("Rakendus läks tööle");
 
-    },
-    bindMouseEvent: function(){
+      //kuulame hiireklikki nupul
+      this.bindMouseEvents();
 
+    },
+    bindMouseEvents: function(){
+      document.querySelector('.add-new-jar').addEventListener('click',this.addNewClick.bind(this));
+    },
+    addNewClick: function(event){
+      console.log(event);
+      this.click_count++;
+      console.log(this.click_count);
     }
   };
 
-  var app = new Moosipurk();
+  window.onload=function(){
+    var app = new Moosipurk();
+  };
 
 
 
